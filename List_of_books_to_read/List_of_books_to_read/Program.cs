439afebd80026daccac0a744b56c257f7cd6092b
nglsnkrs <nglsnkrs@DESKTOP-6HM5FL4>
2025-04-16 21:46:03 +0300

    List_of_books_to_read

---
+++ List_of_books_to_read/List_of_books_to_read/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualBasic;
+
+namespace List_of_books_to_read
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadingList list = new ReadingList();
+            string NameBook;
+
+            while (true) 
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Delete Book");
+                Console.WriteLine("3. Check the book availability");
+                Console.WriteLine("4. Get a book by index");
+                Console.WriteLine("5. Print all list");
+                Console.Write("Your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the number of books to add: ");
+                        int count = Convert.ToInt32(Console.ReadLine());
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            Console.Write($"Enter a title for {i + 1} books: ");
+                            NameBook = Console.ReadLine();
+                            list += NameBook;
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.Write("Enter the name of the book to delete: ");
+                        NameBook = Console.ReadLine();
+                        list -= NameBook;
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.Write("Enter the name of the book to check for availability: ");
+                        NameBook = Console.ReadLine();
+                        Console.WriteLine(list.Contains(NameBook) ? "The book has been found" : "The book was not found");
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.Write("Enter the index to get the book title: ");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        try
+                        {
+                            Console.WriteLine($"Book: {list[index]}");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("List of books to read");
+                        list.PrintList();
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ List_of_books_to_read/List_of_books_to_read/ReadingList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_of_books_to_read
+{
+    public class ReadingList
+    {
+        private List<string> books = new List<string>();
+        public int Count => books.Count;
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < books.Count)
+                    return books[index];
+                else
+                    throw new IndexOutOfRangeException("No book with this index was found.");
+            }
+            set
+            {
+                if (index >= 0 && index < books.Count)
+                    books[index] = value;
+                else
+                    throw new IndexOutOfRangeException("No book with this index was found.");
+            }
+        }
+        public bool Contains(string book)
+        {
+            return books.Contains(book);
+        }
+        public static ReadingList operator +(ReadingList list, string book)
+        {
+            if (!list.Contains(book))
+            {
+                list.books.Add(book);
+                Console.WriteLine("The book has been added");
+            }
+            else
+                Console.WriteLine($"The book \"{book}\" has already been added.");
+            return list;
+        }
+
+        public static ReadingList operator -(ReadingList list, string book)
+        {
+            if (list.Contains(book))
+            {
+                list.books.Remove(book);
+                Console.WriteLine("The book was deleted");
+            }
+            else
+                Console.WriteLine($"The book \"{book}\" was not found in the list.");
+            return list;
+        }
+
+        public void PrintList()
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("The list is empty.");
+                return;
+            }
+    
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {books[i]}");
+            }
+        }
+    }
+}
