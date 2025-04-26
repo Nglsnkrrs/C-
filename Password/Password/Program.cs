18cb93456a14fd223731743a4c3888170b0ef3eb
nglsnkrs <nglsnkrs@DESKTOP-6HM5FL4>
2025-04-26 13:37:41 +0300

    Password

---
+++ Password/Password/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace Password
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "password.txt";
+
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@_-])[A-Za-z\d$@_-]+$");
+            string[] text = File.ReadAllLines(fileName);
+            
+            string resultFolder = "Result";
+            if (!Directory.Exists(resultFolder))
+            {
+                Directory.CreateDirectory(resultFolder);
+            }
+
+            string resultFileName = "good_passwords.txt";
+            string filePath = Path.Combine(resultFolder, resultFileName);
+
+            foreach (var s in text)
+            {
+                if (regex.IsMatch(s))
+                {
+                    File.AppendAllText(filePath, s + Environment.NewLine);
+                    Console.WriteLine("The password is written in a file");
+                }
+            } 
+           
+        }
+    }
+}
