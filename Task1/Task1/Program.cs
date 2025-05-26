3cd9ddd68e16f58f44686282a2e0b5933b37d33d
nglsnkrs <nglsnkrs@DESKTOP-6HM5FL4>
2025-05-26 11:28:49 +0300

    Task 1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            string[] messages = new string[]
+        {
+            "Имя: Тернавская Валерия\nВозраст: 20 лет",
+            "Обучение на курсах в Академии TOP, КФУ им.Вернадского: \nОпыт: 2 года C++, 6 мес. C#, 6 мес. HTML",
+            "Навыки: C#, WinForms, C++, HTML + CSS, Python\nУвлечения: гитра, рисование"
+        };
+
+            int totalChars = 0;
+
+            for (int i = 0; i < messages.Length; i++)
+            {
+                totalChars += messages[i].Length;
+
+                if (i == messages.Length - 1)
+                {
+                    double avg = (double)totalChars / messages.Length;
+                    MessageBox.Show(messages[i], $"Среднее: {Math.Round(avg)} символов");
+                }
+                else
+                {
+                    MessageBox.Show(messages[i], $"Страница {i + 1}");
+                }
+            }
+        }
+    }
+}
