485714d78edcf4ccce149484c601b804d3c88dca
Nglsnkrrs <ternavsdop@gmail.com>
2026-01-14 22:07:23 +0300

    Task_4_process

---
+++ Task_4_process/Task_4_process/Child.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_process
+{
+    class Child
+    {
+        public static int Run(string[] args)
+        {
+            string filePath = args[1];
+            string word = args[2];
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден.");
+                return -1;
+            }
+
+            string text = File.ReadAllText(filePath);
+
+            int count = 0;
+            int index = 0;
+
+            while ((index = text.IndexOf(word, index,
+                   StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                count++;
+                index += word.Length;
+            }
+
+            Console.WriteLine(
+                $"Слово \"{word}\" встречается {count} раз(а).");
+
+            return 0;
+        }
+    }
+}
---
+++ Task_4_process/Task_4_process/Parent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_process
+{
+    class Parent
+    {
+        public static void Run()
+        {
+            Console.Write("Введите путь к файлу: ");
+            string filePath = Console.ReadLine();
+
+            Console.Write("Введите слово для поиска: ");
+            string word = Console.ReadLine();
+
+            Process process = new Process();
+
+            process.StartInfo.FileName =
+                Process.GetCurrentProcess().MainModule.FileName;
+
+            process.StartInfo.Arguments =
+                $"child \"{filePath}\" {word}";
+
+            process.StartInfo.UseShellExecute = false;
+
+            process.Start();
+            process.WaitForExit();
+
+            Console.ReadLine();
+        }
+    }
+}
